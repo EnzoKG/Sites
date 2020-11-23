c656a118a8c976f25c1c0d5c774d15376b28b918
Breno Barros <brenowbarros21@gmail.com>
2020-11-22 20:50:47 -0400

    Adicionando CSS :)

--- contatos-web/public/pages/templates/cabecalho.php
+++ contatos-web/public/pages/templates/cabecalho.php
@@ -3,7 +3,7 @@
     
     if(!isset($tagLinkLogin))
     {
-        $tagLinkLogin = '<a href="'.rotaPages('/../form_login.php').'">Efetue o Login</a>';
+        $tagLinkLogin = '<a class="btn btn-primary btn-login" href="'.rotaPages('/../form_login.php').'">Efetue o Login</a>';
     }
 ?>
 <!DOCTYPE html>
@@ -13,10 +13,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contatos</title>
     <link rel="stylesheet" href="<?=rotaBase('public/assets/css/style.css');?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
 <nav class="menu">
     <ul>
-        <li><?=$tagLinkLogin?></li>
+        <li class><?=$tagLinkLogin?></li>
     </ul>
 </nav>
+
+<div class="container">
+
+</div>
